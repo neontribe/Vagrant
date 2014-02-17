0c949c13e7c24c09200230c063e2678b9ad09fb8
tobybatch <tobias@neontribe.co.uk>
2014-02-17 15:55:06 +0000

    Update apt-get-update.sh

--- bin/apt-get-update.sh
+++ bin/apt-get-update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cat >/etc/apt/sources.list << EOL
+  deb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse
+  deb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse
+  deb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse
+  deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse
+EOL
+
 NOW=`date +%s`
 THEN=`date -r /var/cache/apt +%s`
 ELAPSED=$(($NOW - $THEN))
